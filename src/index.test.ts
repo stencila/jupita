7e27a0531c39cd11ec019f1c55ed83dcec17557b
Nokome Bentley <nokome@stenci.la>
2020-10-27 21:40:01 +1300

    test(*): Add more tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,11 +3,51 @@ import { schema } from '@stencila/executa'
 
 jest.setTimeout(60000)
 
-test('Jupita', async () => {
+test('manifest', async () => {
   const jupita = new Jupita()
 
+  expect(await jupita.manifest()).toEqual(
+    expect.objectContaining({
+      capabilities: expect.objectContaining({
+        manifest: true,
+        execute: expect.objectContaining({
+          properties: expect.objectContaining({
+            node: expect.objectContaining({
+              properties: expect.objectContaining({
+                programmingLanguage: expect.objectContaining({
+                  enum: expect.arrayContaining(['python']),
+                }),
+              }),
+            }),
+          }),
+        }),
+      }),
+    })
+  )
+
+  await jupita.stop()
+})
+
+test('execute', async () => {
+  const jupita = new Jupita()
+  let chunk, expr
+
+  // Attempt to execute a non existent language
+  chunk = await jupita.execute(
+    schema.codeChunk({
+      text: 'foo',
+      programmingLanguage: 'foo',
+    })
+  )
+  expect(chunk.errors).toEqual([
+    schema.codeError({
+      errorMessage:
+        'Jupyter kernel for language "foo" not available on this machine',
+    }),
+  ])
+
   // Execute expression
-  let expr = await jupita.execute(
+  expr = await jupita.execute(
     schema.codeExpression({
       text: '2 * 2 - 1',
       programmingLanguage: 'python',
@@ -28,7 +68,7 @@ test('Jupita', async () => {
   ])
 
   // Execute block returning a JSONable console result
-  let chunk = await jupita.execute(
+  chunk = await jupita.execute(
     schema.codeChunk({
       text: 'print(22)\n6 * 7\n',
       programmingLanguage: 'python',
@@ -72,7 +112,7 @@ plt.show()
   // Adding `%matplotlib inline` currently doesn't work as expected
   // assert.equal(chunk.outputs[0].value.type, 'image')
 
-  // Execute block with error
+  // Execute code chunk with error
   chunk = await jupita.execute(
     schema.codeChunk({
       text: 'foo',
@@ -81,6 +121,20 @@ plt.show()
   )
   expect(chunk.errors).toEqual([
     schema.codeError({ errorMessage: "NameError: name 'foo' is not defined" }),
+  ])
+
+  // Execute code chunk with different language
+  chunk = await jupita.execute(
+    schema.codeChunk({
+      text: '2*2',
+      programmingLanguage: 'haskell',
+    })
+  )
+  expect(chunk.errors).toEqual([
+    schema.codeError({
+      errorMessage:
+        'Language of node (haskell) does not match that of kernel (python)',
+    }),
   ])
 
   await jupita.stop()
